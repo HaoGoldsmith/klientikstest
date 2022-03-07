c9c12e340c24a8b525346ccf4b0cca64d1e8bf37
HaoGoldsmith <68143285+HaoGoldsmith@users.noreply.github.com>
2022-03-07 11:48:07 +0300

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -8,12 +8,11 @@ params_add_client = {'phone': '74445550011', 'first_name': 'Иван', 'patron_n
                      'email': 'test@test.com', 'client_groups': 'Новенький', 'living_address': 'Невский проспект, 25',
                      'sex': '1', 'appointment_confirmation_sms': '1', 'lead_source': 'бот'}
 resp_client = requests.post(url_add_сlient, data=params_add_client)
-print(requests.get(url_add_сlient, params=params_add_client))
 print(resp_client.json())
 
 #функция
-def add_new_clients(client,bot_variables,phone,first_name,patron_name,second_name,email,living_address,sex,
-                    appointment_confirmation_sms,lead_source,client_groups):
+def add_new_clients(client,bot_variables,phone,first_name,patron_name=None,second_name=None,email=None,living_address=None,
+                    sex=None,appointment_confirmation_sms=None,lead_source=None,client_groups=None):
 #добавить клиента
         try:
                 api_key = client.get_var_val('klientiks_api_key')  # апи ключ в переменной проекта
@@ -24,22 +23,18 @@ def add_new_clients(client,bot_variables,phone,first_name,patron_name,second_nam
                                   return {'status': '0', 'error': 'Missing required variables - phone'}
                         if not first_name:
                             first_name = bot_variables.get('first_name')
+                            if not first_name:
+                                return {'status': '0', 'error': 'Missing required variables - first_name'}
                         if not patron_name:
                             patron_name = bot_variables.get('patron_name')
                         if not second_name:
                             second_name = bot_variables.get('second_name')
                         if not email:
-                                email = bot_variables.get('email')
-                                if not email:
-                                   return {'status': '0', 'error': 'Missing required variables - email'}
+                            email = bot_variables.get('email')
                         if not client_groups:
                             client_groups = bot_variables.get('client_groups')
-                            if not client_groups:
-                                return {'status': '0', 'error': 'Missing required variables - client_groups'}
                         if not living_address:
                             living_address = bot_variables.get('living_address')
-                            if not living_address:
-                                   return {'status': '0', 'error': 'Missing required variables - living_address'}
                         if not sex:
                             sex = bot_variables.get('sex')
                         if not appointment_confirmation_sms:
@@ -56,7 +51,8 @@ def add_new_clients(client,bot_variables,phone,first_name,patron_name,second_nam
                                            'sex': 'sex',  #1 - male, 0 - female
                                            'appointment_confirmation_sms': 'appointment_confirmation_sms', #1 - yes, 0 - no
                                            'lead_source': 'lead_source'} # where info from
-                        status, result = main_api_request(api_key, 'new_client_Create', params_add_client)  # вызов функции в ней просто запрос к апи (тут можешь просто свой вызов добавить)
+                        status, result = requests.post(api_key, data=params_add_client)
+                        # status, result = main_api_request(api_key, 'new_client_Create', params_add_client)  # вызов функции в ней просто запрос к апи (тут можешь просто свой вызов добавить)
                         if status:
                             answer = {'status': '1', 'result': True}
                         else:
